53982603a19acb0dc213f4d9d306afc7e5b724a4
out0f4mm0 <eriking2004@gmail.com>
2023-09-29 09:23:16 +0200

    Minor changes on exercise 3

--- js/Ejercicio3.js
+++ js/Ejercicio3.js
@@ -1,10 +1,15 @@
 const name = prompt("Buenas! como te llamas?")
-const hour = parseInt(prompt("Que hora es?"))
+let hour = prompt("Que hora es?")
 
-if(hour >= 5 && hour <=11){
-    alert(`Buenos dias ${name}`)
-}else if(hour > 11 && hour <= 19){
-    alert(`Buenas tardes ${name}`)
-}else {
-    alert(`Buenas noches ${name}`)
+if(hour < 0 || hour > 24 || isNaN(hour)){
+    alert("La hora introducida no es valida")
+}else{
+    hour = parseInt(hour)
+    if(hour >= 5 && hour <=11){
+        alert(`Buenos dias ${name}`)
+    }else if(hour > 11 && hour <= 19){
+        alert(`Buenas tardes ${name}`)
+    }else {
+        alert(`Buenas noches ${name}`)
+    }
 }
